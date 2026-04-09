edbd427f530e76a08cca524c107f22fa96378570
Debesh Panda <debeshpanda@Debeshs-MacBook-Air.local>
2026-04-09 19:32:17 +0530

    Some minor fixes

--- src/data/developer.ts
+++ src/data/developer.ts
@@ -16,12 +16,12 @@ const Developer = [
         e_mail: "itsdkp555@gmail.com",
     },
     {
-        img: "/imgs/sunil.jpg",
+        img: "/imgs/Akash.jpg",
         name: "Akash Kumar Gouda",
         position: "Developer",
-        insta: "https://www.instagram.com/its_sunilb?igsh=MWw4c3NvcDN1OXYwNQ==",
+        insta: "",
         linkdin: "https://www.linkedin.com/in/sunil-kumar-bal?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ",
-        e_mail: "balsunilkumar34@gmail.com",
+        e_mail: "akashgouda772@gmail.com",
     },
 ]
 
